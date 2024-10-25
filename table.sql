8d9cf260f67faa657dd98c98da30cad05d89255a
Olamide Olaniyan <olaniyanolamide42@hotmail.com>
2024-10-25 15:17:05 +0100

    update sql

--- table.sql
+++ table.sql
@@ -1,9 +1,14 @@
 -- Create the users table
 CREATE TABLE IF NOT EXISTS users (
-  id bigint PRIMARY KEY,
-  name text,
-  url text,
-  subscribers int
+    id bigint PRIMARY KEY,
+    name text,
+    url text,
+    subscribers int,
+    author_name text,
+    author_handle text,
+    author_photo_url text,
+    author_bio text,
+    twitter_screen_name text
 );
 
 -- Create the posts table
@@ -29,49 +34,72 @@ CREATE TABLE IF NOT EXISTS posts (
 -- Fetch the 10 Latest Posts for All Users
 SELECT *
 FROM posts
-WHERE user_id IN (SELECT id FROM users)
+WHERE
+    user_id IN (
+        SELECT id
+        FROM users
+    )
 ORDER BY post_date DESC
 LIMIT 10;
 
 -- Fetch lastest 10 posts for each user
-WITH ranked_posts AS (
-  SELECT *,
-         ROW_NUMBER() OVER (PARTITION BY user_id ORDER BY post_date DESC) AS rn
-  FROM posts
-)
+WITH
+    ranked_posts AS (
+        SELECT *, ROW_NUMBER() OVER (
+                PARTITION BY
+                    user_id
+                ORDER BY post_date DESC
+            ) AS rn
+        FROM posts
+    )
 SELECT *
 FROM ranked_posts
-WHERE rn <= 10;
+WHERE
+    rn <= 10;
 
 -- Fetch the 10 Most Popular Posts for Each User Based on "Likes"
-WITH ranked_posts AS (
-  SELECT *,
-         ROW_NUMBER() OVER (PARTITION BY user_id ORDER BY likes DESC) AS rn
-  FROM posts
-)
+WITH
+    ranked_posts AS (
+        SELECT *, ROW_NUMBER() OVER (
+                PARTITION BY
+                    user_id
+                ORDER BY likes DESC
+            ) AS rn
+        FROM posts
+    )
 SELECT *
 FROM ranked_posts
-WHERE rn <= 10;
+WHERE
+    rn <= 10;
 
 -- Fetch Latest 10 Newsletter Posts for Each User
 SELECT *
 FROM (
-    SELECT p.*, 
-           ROW_NUMBER() OVER (PARTITION BY p.user_id ORDER BY p.post_date DESC) AS rn
-    FROM posts p
-    WHERE p.type = 'newsletter'
-) subquery
-WHERE rn <= 10
+        SELECT p.*, ROW_NUMBER() OVER (
+                PARTITION BY
+                    p.user_id
+                ORDER BY p.post_date DESC
+            ) AS rn
+        FROM posts p
+        WHERE
+            p.type = 'newsletter'
+    ) subquery
+WHERE
+    rn <= 10
 ORDER BY user_id, post_date DESC;
 
 -- Fetch Latest 10 Podcast Posts for Each User
 SELECT *
 FROM (
-    SELECT p.*, 
-           ROW_NUMBER() OVER (PARTITION BY p.user_id ORDER BY p.post_date DESC) AS rn
-    FROM posts p
-    WHERE p.type = 'podcast'
-) subquery
-WHERE rn <= 10
+        SELECT p.*, ROW_NUMBER() OVER (
+                PARTITION BY
+                    p.user_id
+                ORDER BY p.post_date DESC
+            ) AS rn
+        FROM posts p
+        WHERE
+            p.type = 'podcast'
+    ) subquery
+WHERE
+    rn <= 10
 ORDER BY user_id, post_date DESC;
-
